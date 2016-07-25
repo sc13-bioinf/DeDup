aea132ad8b4d38f897a560678bbd2b99f2a339a2
Stephen Clayton <clayton@shh.mpg.de>
2016-07-25 11:14:13 +0200

    fix bug when read names have M_ or F_ or R_ and allReadAsMerged is set

--- src/RMDupper.java
+++ src/RMDupper.java
@@ -258,45 +258,48 @@ public class RMDupper{
           boolean duplicateIsShorterOrEqual = maybeDuplicate.middle - maybeDuplicate.left <= recordBuffer.peekFirst().middle - recordBuffer.peekFirst().left;
           boolean duplicateIsLongerOrEqual = recordBuffer.peekFirst().middle - recordBuffer.peekFirst().left <= maybeDuplicate.middle - maybeDuplicate.left;
 
-          if ( allReadsAsMerged &&
-               recordBuffer.peekFirst().left.equals(maybeDuplicate.left)  &&
-               recordBuffer.peekFirst().middle.equals(maybeDuplicate.middle) ) {
+          if ( allReadsAsMerged ) {
+            if ( recordBuffer.peekFirst().left.equals(maybeDuplicate.left)  &&
+                 recordBuffer.peekFirst().middle.equals(maybeDuplicate.middle) ) {
                  //System.out.println("* add");
                  duplicateBuffer.add(maybeDuplicate);
-          } else if ( recordBuffer.peekFirst().right.getReadName().startsWith("M_") &&
-               ( ( maybeDuplicate.right.getReadName().startsWith("M_") &&
-                   recordBuffer.peekFirst().left.equals(maybeDuplicate.left)  &&
-                   recordBuffer.peekFirst().middle.equals(maybeDuplicate.middle) ) ||
-                 ( maybeDuplicate.right.getReadName().startsWith("F_") &&
-                   recordBuffer.peekFirst().left.equals(maybeDuplicate.left) &&
-                   duplicateIsShorterOrEqual ) ||
-                 ( maybeDuplicate.right.getReadName().startsWith("R_") &&
-                   recordBuffer.peekFirst().middle.equals(maybeDuplicate.middle) &&
-                   duplicateIsShorterOrEqual ) ) ) {
-                   //System.out.println("M_ add");
-               duplicateBuffer.add(maybeDuplicate);
-          } else if ( recordBuffer.peekFirst().right.getReadName().startsWith("F_") &&
-                      ( ( maybeDuplicate.right.getReadName().startsWith("M_") &&
-                          recordBuffer.peekFirst().left.equals(maybeDuplicate.left) &&
-                          duplicateIsLongerOrEqual ) ||
-                        ( maybeDuplicate.right.getReadName().startsWith("F_") &&
-                          recordBuffer.peekFirst().left.equals(maybeDuplicate.left) ) ||
-                        ( maybeDuplicate.right.getReadName().startsWith("R_") &&
-                          recordBuffer.peekFirst().middle.equals(maybeDuplicate.middle) &&
-                          duplicateIsShorterOrEqual ) ) ) {
-                      //System.out.println("F_ add");
-             duplicateBuffer.add(maybeDuplicate);
-          } else if ( recordBuffer.peekFirst().right.getReadName().startsWith("R_") &&
-                      ( ( maybeDuplicate.right.getReadName().startsWith("M_") &&
-                          recordBuffer.peekFirst().middle.equals(maybeDuplicate.middle) &&
-                          duplicateIsLongerOrEqual ) ||
-                        ( maybeDuplicate.right.getReadName().startsWith("F_") &&
-                          recordBuffer.peekFirst().left.equals(maybeDuplicate.left) &&
-                          recordBuffer.peekFirst().middle.equals(maybeDuplicate.middle) ) ||
-                        ( maybeDuplicate.right.getReadName().startsWith("R_") &&
-                          recordBuffer.peekFirst().middle.equals(maybeDuplicate.middle) ) ) ) {
-            //System.out.println("R_ add");
-             duplicateBuffer.add(maybeDuplicate);
+            }
+          } else {
+            if ( recordBuffer.peekFirst().right.getReadName().startsWith("M_") &&
+                 ( ( maybeDuplicate.right.getReadName().startsWith("M_") &&
+                     recordBuffer.peekFirst().left.equals(maybeDuplicate.left)  &&
+                     recordBuffer.peekFirst().middle.equals(maybeDuplicate.middle) ) ||
+                   ( maybeDuplicate.right.getReadName().startsWith("F_") &&
+                     recordBuffer.peekFirst().left.equals(maybeDuplicate.left) &&
+                     duplicateIsShorterOrEqual ) ||
+                   ( maybeDuplicate.right.getReadName().startsWith("R_") &&
+                     recordBuffer.peekFirst().middle.equals(maybeDuplicate.middle) &&
+                     duplicateIsShorterOrEqual ) ) ) {
+              //System.out.println("M_ add");
+              duplicateBuffer.add(maybeDuplicate);
+            } else if ( recordBuffer.peekFirst().right.getReadName().startsWith("F_") &&
+                        ( ( maybeDuplicate.right.getReadName().startsWith("M_") &&
+                            recordBuffer.peekFirst().left.equals(maybeDuplicate.left) &&
+                            duplicateIsLongerOrEqual ) ||
+                          ( maybeDuplicate.right.getReadName().startsWith("F_") &&
+                            recordBuffer.peekFirst().left.equals(maybeDuplicate.left) ) ||
+                          ( maybeDuplicate.right.getReadName().startsWith("R_") &&
+                            recordBuffer.peekFirst().middle.equals(maybeDuplicate.middle) &&
+                            duplicateIsShorterOrEqual ) ) ) {
+              //System.out.println("F_ add");
+              duplicateBuffer.add(maybeDuplicate);
+            } else if ( recordBuffer.peekFirst().right.getReadName().startsWith("R_") &&
+                        ( ( maybeDuplicate.right.getReadName().startsWith("M_") &&
+                            recordBuffer.peekFirst().middle.equals(maybeDuplicate.middle) &&
+                            duplicateIsLongerOrEqual ) ||
+                          ( maybeDuplicate.right.getReadName().startsWith("F_") &&
+                            recordBuffer.peekFirst().left.equals(maybeDuplicate.left) &&
+                            recordBuffer.peekFirst().middle.equals(maybeDuplicate.middle) ) ||
+                          ( maybeDuplicate.right.getReadName().startsWith("R_") &&
+                            recordBuffer.peekFirst().middle.equals(maybeDuplicate.middle) ) ) ) {
+              //System.out.println("R_ add");
+              duplicateBuffer.add(maybeDuplicate);
+            }
           }
         }
         /* DEBUG
